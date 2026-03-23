7c74f71ea1ec5e9a0066f3e7d4badf913712a6b8
Miraç Göksu Öztürk <miracgoksuozturk@gmail.com>
2026-03-23 15:19:26 +0300

    search binding changed

--- init.lua
+++ init.lua
@@ -441,8 +441,13 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sc', builtin.commands, { desc = '[S]earch [C]ommands' })
-      vim.keymap.set('n', '<leader>shf', function() builtin.find_files { hidden = true } end, { desc = '[S]earch [H]idden [F]iles' })
-      vim.keymap.set('n', '<leader>shg', function() builtin.live_grep { additional_args = { '--hidden' } } end, { desc = '[S]earch [H]idden by [G]rep' })
+      vim.keymap.set('n', '<leader>saf', function() builtin.find_files { hidden = true } end, { desc = '[S]earch [A]ll [F]iles including hidden files' })
+      vim.keymap.set(
+        'n',
+        '<leader>sag',
+        function() builtin.live_grep { additional_args = { '--hidden' } } end,
+        { desc = '[S]earch [A]ll by [G]rep including hidden files' }
+      )
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- This runs on LSP attach per buffer (see main LSP attach function in 'neovim/nvim-lspconfig' config for more info,
